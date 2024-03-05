d12e4a798a14981819db331bae71d828fae37aa2
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-03-06 06:36:50 +0700

    Replace Character , Add charater rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -29,5 +29,7 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * Time.deltaTime * moveSpeed;
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward,moveDir, Time.deltaTime * rotationSpeed);
     }
 }
