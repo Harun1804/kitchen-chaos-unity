93c4c51b5d268b773dfeb3ba29fe6acd1509edc8
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-03-27 05:41:05 +0700

    Player can pick & drop object

--- Assets/Scripts/Counter/ClearCounter.cs
+++ Assets/Scripts/Counter/ClearCounter.cs
@@ -8,6 +8,23 @@ public class ClearCounter : BaseCounter
     
     public override void Interact(Player player)
     {
-        
+        if (!HasKitchenObject()) {
+            // There no kitchen object on the counter
+            if (player.HasKitchenObject()) {
+                // player is carrying a kitchen object
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else {
+                // player is not carrying a kitchen object
+            }
+        } else {
+            // There is a kitchen object on the counter
+            if (player.HasKitchenObject()) {
+                // player is carrying a kitchen object
+                
+            } else {
+                // player is not carrying a kitchen object
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
